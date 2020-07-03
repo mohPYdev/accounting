795b1d31ba7b99974181266fd540fe98424b37c1
mohamad <rafieiyan.moh@gmail.com>
2020-07-03 17:53:29 +0430

    created Bill , Instrument , Salary classes

--- src/costumer/Bill.java
+++ src/costumer/Bill.java
@@ -1,4 +1,35 @@
 package costumer;
 
 public class Bill {
+    public enum BillType{
+        WATER , GAS , POWER , TELEPHONE
+    }
+
+    String id , price;
+    String date;  //    year/month/day
+    BillType type;
+
+    Bill(String id , String price , String date , BillType type)
+    {
+        this.id = id;
+        this.price = price;
+        this.date = date;
+        this.type = type;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public BillType getType() {
+        return type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
--- src/costumer/Salary.java
+++ src/costumer/Salary.java
@@ -1,4 +1,25 @@
 package costumer;
 
 public class Salary {
+
+    String date;
+    String price , emp_name;
+
+    Salary(String date , String price , String emp_name){
+        this.date=  date;
+        this.price = price;
+        this.emp_name = emp_name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getEmp_name() {
+        return emp_name;
+    }
 }
