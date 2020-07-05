b13bf411416c5b52c3f528077b346993ea486a64
Alireza_Tabatabaei <alirezatabatabaei100@gmail.com>
2020-07-06 02:15:52 +0430

    Merge pull request #3 from mohPYdev/testing
    created Factory , Costumer classes

--- src/costumer/Municipality.java
+++ src/costumer/Municipality.java
@@ -2,9 +2,22 @@ package costumer;
 
 import java.util.ArrayList;
 
-public class Municipality  extends Costumer{
+public class Municipality extends Costumer{
 
-    Municipality(String name, String email, String phone_number, String activity_type, ArrayList<String> employee_names) {
+    private String mayor_name;
+    private int region;
+
+    public String getMayor_name(){return mayor_name;}
+    public int getRegion(){return region;}
+    public void setMayor_name(String mayor_name){this.mayor_name=mayor_name;}
+    public void setRegion(int region){this.region=region;}
+
+    Municipality(String name, String email, String phone_number
+            , String activity_type, ArrayList<String> employee_names,String mayor_name,int region) {
+
         super(name, email, phone_number, activity_type, employee_names);
+
+        this.mayor_name=mayor_name;
+        this.region=region;
     }
 }
